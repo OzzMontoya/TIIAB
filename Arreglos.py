797b0a242996a376da013c4e69b9cca43b42dcd3
OzzMontoya <utm24090690@utma.edu.mx>
2025-05-27 14:31:34 -0600

    nuevo cambio

---
+++ Arreglos.py
@@ -0,0 +1,55 @@
+""" Arreglos """
+
+""" new_list = []
+mi_lista = [1,2,3,4,5]
+
+for i in mi_lista:
+    i = i * 2
+    new_list.append(i)
+print(new_list)
+ """
+
+
+#2
+""" n1 = input("Ingrese el primer nombre: ")
+n2 = input("Ingrese el segundo nombre: ")
+n3 = input("Ingrese el tercer nombre: ")
+nombres = [n1, n2, n3]
+nombres.reverse()
+print(nombres)
+ """
+
+#3
+""" n1 = int(input("Ingrese el primer número: "))
+n2 = int(input("Ingrese el segundo número: "))
+n3 = int(input("Ingrese el tercer número: "))
+n4 = int(input("Ingrese el cuarto número: "))
+n5 = int(input("Ingrese el quinto número: "))
+lista = [n1, n2, n3, n4, n5]
+pares = []
+for i in lista:
+    if i % 2 == 0:
+        pares.append(i)
+print(pares) """
+
+#4
+""" n1 = int(input("Ingrese el primer número: "))
+n2 = int(input("Ingrese el segundo número: "))
+n3 = int(input("Ingrese el tercer número: "))
+n4 = int(input("Ingrese el cuarto número: "))
+n5 = int(input("Ingrese el quinto número: "))
+n6 = int(input("Ingrese el sexto número: "))
+n7 = int(input("Ingrese el séptimo número: "))
+n8 = int(input("Ingrese el octavo número: "))
+n9 = int(input("Ingrese el noveno número: "))
+n10 = int(input("Ingrese el décimo número: "))
+lista = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10] 
+pares = []
+impares = []
+for i in lista:
+    if i % 2 == 0:
+        pares.append(i)
+    else:
+        impares.append(i)
+print("Pares:", pares)
+print("Impares:", impares) """
